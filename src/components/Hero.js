4c485c8c5c259413a36e68cd958c3cd382189e1b
Mwambuka <bertinmwambuka@gmail.com>
2023-02-13 15:55:12 +0530

    link html modification

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -17,22 +17,22 @@ const Hero = () => {
                 <h3>Software Engineer</h3>
                 <div className='btn-container'>
                     <a href='#contact' className='btn btn-hire'>Reach out</a>
-                    <a href='https://drive.google.com/file/d/1KV2hMVh-NhYcHmsjRkvtn_m37WWBcBI3/view?usp=sharing' className='btn btn-resume'>Resume</a>
+                    <a href='https://drive.google.com/file/d/1KV2hMVh-NhYcHmsjRkvtn_m37WWBcBI3/view?usp=sharing' target="_blank" className='btn btn-resume'>Resume</a>
                 </div>
                 <div className='social-medias'>
-                    <a href="https://github.com/Mwambuka" className='icons'>
+                    <a href="https://github.com/Mwambuka" target="_blank" className='icons'>
                         <FaGithub  className='icon'/>
                     </a>
-                    <a href='https://www.linkedin.com/in/bertin-mwambuka-9a645b201/' className='icons'>
+                    <a href='https://www.linkedin.com/in/bertin-mwambuka-9a645b201/' target="_blank" className='icons'>
                         <FaLinkedin className='icon' />
                     </a>
-                    <a href="https://twitter.com/bertinmwambuka" className='icons'>
+                    <a href="https://twitter.com/bertinmwambuka" target="_blank" className='icons'>
                         <FaTwitterSquare  className='icon'/>
                     </a>
-                    <a href="https://www.instagram.com/bertinmwambuka/" className='icons'>
+                    <a href="https://www.instagram.com/bertinmwambuka/" target="_blank" className='icons'>
                         <FaInstagram className='icon' />
                     </a>
-                    <a href="https://www.facebook.com/bertin.nicholas.7" className='icons'>
+                    <a href="https://www.facebook.com/bertin.nicholas.7" target="_blank" className='icons'>
                         <FaFacebookF className='icon' />
                     </a>
                 </div>
